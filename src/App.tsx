1d584195eb3217929820486c0b5dea794622e70f
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-29 11:15:02 +0200

    Initial App styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="">
+      <h1 className="text-center text-4xl text-gray-400 font-bold my-7">Todos</h1>
+      <form className="relative mx-12">
+        <input type="text" className="px-4 py-4 shadow-xl rounded-full w-full placeholder-gray-900 border-2 border-gray-300" placeholder='Add a todo' />
+        <button className="absolute top-4 right-8">
+          <FontAwesomeIcon icon={faPlusCircle} className='text-green-600 text-2xl'/>
+        </button>
+      </form>
+    </main>
   )
 }
