7b8aa05d52fa7c2e85ebfca15fdeffe8748664dd
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-29 14:19:36 +0200

    Todo: Add functionality to add notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,57 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 import Todo from './components/Todo'
-import { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
 import { nanoid } from 'nanoid';
 
-export default function App() {
+export default function App(): JSX.Element {
 
+  // types
+
+  type Tobe = {
+    id: string;
+    title: string;
+    createdAt: Date;
+    isCompleted: boolean;
+  }
   
   const [todoInput, setTodoInput] = useState('');
+  const [todos, setTodos] = useState<Tobe[]>([])
 
-  const handleInput = (e): ChangeEvent<HTMLInputElement> => {
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
     setTodoInput(e.target.value);
   }
   
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if(todoInput.trim() === "") return;
+
+    const newTodo: Tobe = {
+      id: nanoid(),
+      title: todoInput,
+      createdAt: new Date(),
+      isCompleted: false,
+    }
+
+    setTodos(prevTodos => [...prevTodos, newTodo]);
+
+    setTodoInput('')
+
+    
+  }
+
+  console.log(todos)
   return (
     <main className="mx-12">
       <h1 className="text-center text-4xl text-gray-400 font-bold my-7">Todos</h1>
-      <form className="relative mb-4">
-        <input type="text" className="px-4 py-4 shadow-xl rounded-full w-full placeholder-gray-900 border-2 border-gray-300" placeholder='Add a todo' onChange={(e) => handleInput(e)} />
+      <form className="relative mb-4" onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" className="px-4 py-4 shadow-xl rounded-full w-full placeholder-gray-900 border-2 border-gray-300" placeholder='Add a todo' id='todo-input' value={todoInput} onChange={(e) => handleInput(e)} />
         <button className="absolute top-4 right-8">
           <FontAwesomeIcon icon={faPlusCircle} className='text-green-600 text-2xl'/>
         </button>
       </form>
-      <Todo/>
+      <Todo todos={todos}/>
     </main>
   )
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,20 +1,34 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export default function Todo() {
+
+type Tobe = {
+    id: string;
+    title: string;
+    createdAt: Date;
+    isCompleted: boolean;
+}
+
+export default function Todo({todos}: {todos: Tobe[]}) {
+    const doing = todos.map((tada) => (
+        <li className="flex space-x-4" id={tada.id}>
+        <button className="">
+            <FontAwesomeIcon icon={faSquare} className='text-gray-300'/>
+        </button>
+        <p className="w-[95%]">{tada.title}</p>
+        <button className="">
+            <FontAwesomeIcon icon={faTrash} className='text-red-600 px-2 py-2 rounded-full bg-gray-200'/>
+        </button>
+    </li>
+    ))
+
+    console.log(doing)
+
     return(
         <div className="">
-            <h1 className="text-gray-400 text-2xl font-semibold text-center">No todos for now...</h1>
+            {todos.length === 0 && <h1 className="text-gray-400 text-2xl font-semibold text-center">No todos for now...</h1>}
             <ul className="flex flex-col space-y-4 py-4">
-                <li className="flex space-x-4">
-                    <button className="">
-                        <FontAwesomeIcon icon={faSquare} className='text-gray-300'/>
-                    </button>
-                    <p className="w-[95%]">I need the prayers of those I love.</p>
-                    <button className="">
-                        <FontAwesomeIcon icon={faTrash} className='text-red-600 px-2 py-2 rounded-full bg-gray-200'/>
-                    </button>
-                </li>
+                {doing}
             </ul>
         </div>
     )
